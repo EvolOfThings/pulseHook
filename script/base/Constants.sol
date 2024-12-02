b45e0c7234e0e5c0eeba65838fdbb0239c19900f
rishad <rishad.yamnoor@gmail.com>
2024-12-02 09:42:07 +0530

    use unichain sepolia addr

--- script/base/Constants.sol
+++ script/base/Constants.sol
@@ -9,8 +9,9 @@ import {IAllowanceTransfer} from "permit2/src/interfaces/IAllowanceTransfer.sol"
 contract Constants {
     address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
 
+    /// @dev populated with Unichain Sepolia addresses
+    IPoolManager constant POOLMANAGER = IPoolManager(address(0xc81462fec8b23319f288047f8a03a57682a35c1a));
+    PositionManager constant posm = PositionManager(payable(address(0xb433cb9bcdf4cfcc5cab7d34f90d1a7deefd27b9)));
     /// @dev populated with default anvil addresses
-    IPoolManager constant POOLMANAGER = IPoolManager(address(0x5FbDB2315678afecb367f032d93F642f64180aa3));
-    PositionManager constant posm = PositionManager(payable(address(0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0)));
     IAllowanceTransfer constant PERMIT2 = IAllowanceTransfer(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
 }
